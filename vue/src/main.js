b8501e084dd53d948a48974223d5208f193ce86b
jjmontalban <jjmontalban@gmail.com>
2020-12-30 11:45:52 +0100

    Intial config + Posts component

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
+
+//Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost/wp-vue/wordpress/wp-json/'
 Vue.config.productionTip = false
 
 new Vue({
