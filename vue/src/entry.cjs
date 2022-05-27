f7dca03f8fc4a7c7e27122295c9816bf6709ea60
JJMontalban <jjmontalban@gmail.com>
2022-05-27 09:56:27 +0200

    Heroku config

--- vue/src/entry.cjs
+++
@@ -1,6 +0,0 @@
-// entry.cjs
-// https://stackoverflow.com/questions/71408615/how-to-launch-a-sveltekit-app-on-plesk-phusion-passenger
-async function loadApp() {
-    const { app } = await import("./main.js");
-}
-loadApp()
